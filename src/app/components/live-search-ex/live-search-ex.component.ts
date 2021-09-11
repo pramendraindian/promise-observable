4cf9bbed41f8d63d1b3cf688021e351399b76302
pramendraindian <pramendra.indian@gmail.com>
2021-09-11 17:48:10 +0530

    added bootstrap

--- src/app/components/live-search-ex/live-search-ex.component.ts
+++ src/app/components/live-search-ex/live-search-ex.component.ts
@@ -14,6 +14,7 @@ import { RxJsTricksService } from 'src/app/services/rx-js-tricks.service';
 export class LiveSearchExComponent implements OnInit {
   myForm: FormGroup ;
   user:User;
+  users:User[]=[];
   constructor(private formBuilder:FormBuilder,private rx:RxJsTricksService) { 
     this.user = {id:0,last_name:'',first_name:'',email:'',avatar:''};
     this.myForm = this.formBuilder.group({
@@ -30,12 +31,23 @@ export class LiveSearchExComponent implements OnInit {
         result=>{//success
             if(result instanceof HttpErrorResponse)
             {
+              this.users=[];
               console.log(result.status)
             }
             else
             {
-              this.user=result as User; 
-              console.log(this.user);
+              if(result?.data?.length)
+              {
+                this.users=[];
+                this.users=result?.data;
+              }
+              else
+              {
+                 this.users=[];
+                  this.user=result.data as User; 
+                  console.log(this.user);
+                  this.users.push(this.user);
+              }
             }
         }
         ,err=>{ // It will never reach in case of switvhMap
